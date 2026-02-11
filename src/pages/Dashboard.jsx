1907480005276a6859d1a12e42d3ebe799738b5b
rayen <hidrirayen99@gmail.com>
2026-02-11 17:13:52 +0100

    refactor: handle loading and error states in project pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,58 +7,75 @@ import useProjects from "../hooks/useProjects";
 
 
 const Dashboard = () => {
-    const {projects, addProject, deleteProject} = useProjects();
+    const {projects, error, loading, addProject, deleteProject} = useProjects();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const navigate = useNavigate();
 
-    return (
-        <div>
-            {isModalOpen && <AddProjectModal onClose={() => setIsModalOpen(false)} onAddProject={addProject} />}
-            <main className="p-6">
-                <div>
-                    <h2 className="text-2xl font-bold mt-6 mb-4 px-6">Welcome to your Dashboard</h2>
-                    <p className="text-gray-600 px-6">Here you can manage your projects and track their progress.</p>
-                </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 mt-6 justify-items-center'>
-                    <StatCard title="Total Projects" stat={projects.length} />
-                    <StatCard title="Ongoing Projects" stat={projects.filter(p => p.status === "In Progress").length} />
-                    <StatCard title="Completed Projects" stat={projects.filter(p => p.status === "Completed").length} />
-                </div>
-                <div className="mt-8 px-6">
-                    <h2 className="text-2xl font-bold mt-6 mb-4">Recent Projects</h2>
-                    {projects.length === 0 ? (
-                    <div className="flex flex-col items-center justify-center text-center py-16 bg-white rounded-lg shadow-sm">
-                        <h3 className="text-xl font-semibold mb-2">
-                            No projects yet
-                        </h3>
-                        <p className="text-gray-600 mb-4">
-                            Create your first project to start tracking progress.
-                        </p>
-                        <button
-                            onClick={() => setIsModalOpen(true)}
-                            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                        >
-                            Add your first project
-                        </button>
+    
+    if (error) {
+        return (
+            <div className="p-6">
+                <div className="text-red-500">{error}</div>
+            </div>
+        );
+    }else if (loading) {
+        return (
+            <div className="p-6">
+                <div className="text-gray-600">Loading projects...</div>
+            </div>
+        );
+    }else {
+        return (
+            <div>
+                {isModalOpen && <AddProjectModal onClose={() => setIsModalOpen(false)} onAddProject={addProject} />}
+                <main className="p-6">
+                    <div>
+                        <h2 className="text-2xl font-bold mt-6 mb-4 px-6">Welcome to your Dashboard</h2>
+                        <p className="text-gray-600 px-6">Here you can manage your projects and track their progress.</p>
                     </div>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-                        {projects.slice(-3).reverse().map((project) => (
-                            <ProjectCard key={project.id} project={project} onDelete={deleteProject}/>
-                        ))}
-                        {
-                            projects.length > 3 && (
-                            <div className="col-span-full text-center mt-4">
-                                <button className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 cursor-pointer" onClick={() => navigate('/projects')}>
-                                    View All Projects
-                                </button>
-                            </div>
-                        )}
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 mt-6 justify-items-center'>
+                        <StatCard title="Total Projects" stat={projects.length} />
+                        <StatCard title="Ongoing Projects" stat={projects.filter(p => p.status === "In Progress").length} />
+                        <StatCard title="Completed Projects" stat={projects.filter(p => p.status === "Completed").length} />
                     </div>
-                )}
-                </div>
-            </main>
-        </div>
-    )
+                    <div className="mt-8 px-6">
+                        <h2 className="text-2xl font-bold mt-6 mb-4">Recent Projects</h2>
+                        {projects.length === 0 ? (
+                        <div className="flex flex-col items-center justify-center text-center py-16 bg-white rounded-lg shadow-sm">
+                            <h3 className="text-xl font-semibold mb-2">
+                                No projects yet
+                            </h3>
+                            <p className="text-gray-600 mb-4">
+                                Create your first project to start tracking progress.
+                            </p>
+                            <button
+                                onClick={() => setIsModalOpen(true)}
+                                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                            >
+                                Add your first project
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                            {projects.slice(-3).reverse().map((project) => (
+                                <ProjectCard key={project.id} project={project} onDelete={deleteProject}/>
+                            ))}
+                            {
+                                projects.length > 3 && (
+                                <div className="col-span-full text-center mt-4">
+                                    <button className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 cursor-pointer" onClick={() => navigate('/projects')}>
+                                        View All Projects
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    </div>
+                </main>
+            </div>
+        )
+    }
+            
+    
 }
 export default Dashboard;
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -6,7 +6,7 @@ import EditProjectModal from "../components/EditProjectModal";
 import { useState } from "react";
 import useProjects from "../hooks/useProjects";
 const ProjectDetails = () => {
-    const {projects, addTask, toggleTask, deleteTask, editProject} = useProjects();
+    const {projects, error, loading, addTask, toggleTask, deleteTask, editProject} = useProjects();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [newTaskTitle, setNewTaskTitle] = useState("")
     const navigate = useNavigate();
@@ -22,13 +22,6 @@ const ProjectDetails = () => {
         addTask(project.id, newTaskTitle);
         setNewTaskTitle("");
     };
-    if (!project) {
-        return (
-            <div className="p-6 flex justify-center items-center h-screen">
-                <p className="text-gray-600 text-lg">Loading...</p>
-            </div>
-        )
-    }
     const tasksProgress = (tasks) => {
         const totalTasks = tasks.length
         if (totalTasks === 0) return 0
@@ -36,57 +29,71 @@ const ProjectDetails = () => {
         const progress = Math.round(completedTasks / totalTasks * 100);
         return progress
     }
-    return (
-         <div className="p-6 max-w-4xl mx-auto">
-            {isModalOpen && <EditProjectModal project={project} onClose={() => setIsModalOpen(false)} onEditProject={editProject} />}
-            <div className="grid grid-cols-2 mb-6 gap-4 items-center">
-            <h2 className="text-2xl font-bold px-6">Project Details</h2>
-            <button className="w-full sm:w-auto bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg transition cursor-pointer"
-                onClick={() => navigate(-1)}
-            >← Back</button>
+    if (error) {
+        return (
+            <div className="p-6">
+                <div className="text-red-500">{error}</div>
             </div>
-            <div className="bg-white rounded-xl shadow-md p-4 sm:p-6 space-y-4">
-                <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-4">
-                    <div className="flex-1 min-w-0">
-                        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800 break-words">{project.name}</h1>
-                        <p className="text-sm sm:text-base text-gray-600 mt-2 break-words">{project.description}</p>
-                        <p className="text-xs sm:text-sm text-gray-500 mt-2">
-                        Created on: <span className="font-medium">{project.createdAt}</span>
-                        </p>
-                    </div>
-                    <div className="gap-3 flex flex-col items-start sm:items-end sm:gap-10 flex-shrink-0">
-                        <span className={`px-3 py-1 rounded-full text-xs sm:text-sm font-semibold whitespace-nowrap
-                            ${project.status === "Completed" ? "bg-green-500 text-white" : 
-                              project.status === "In Progress" ? "bg-blue-500 text-white" : "bg-yellow-500 text-white"}`}>
-                            {project.status}
-                        </span>
-                        <button onClick={() => setIsModalOpen(true)} className="w-full sm:w-auto px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-sm font-semibold">Edit</button>
-                    </div>
+        );
+    }else if (loading) {
+        return (
+            <div className="p-6">
+                <div className="text-gray-600">Loading projects...</div>
+            </div>
+        );
+    }else {
+        return (
+            <div className="p-6 max-w-4xl mx-auto">
+                {isModalOpen && <EditProjectModal project={project} onClose={() => setIsModalOpen(false)} onEditProject={editProject} />}
+                <div className="grid grid-cols-2 mb-6 gap-4 items-center">
+                <h2 className="text-2xl font-bold px-6">Project Details</h2>
+                <button className="w-full sm:w-auto bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg transition cursor-pointer"
+                    onClick={() => navigate(-1)}
+                >← Back</button>
                 </div>
-                
-                
-            </div>
-            <h3 className="text-xl font-bold mt-6 mb-2">Progress</h3>
-            <div className="mt-2 justify-items-center">
-                <ProgressBar value={tasksProgress(project.tasks)} />
-            </div>
-            <div className="mt-8">
-                <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4 gap-4">
-                    <h3 className="text-xl font-bold">Tasks</h3>
-                    <input type="text" placeholder="New Task?" value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} className="w-full sm:flex-1 border rounded-md bg-gray-100 p-2 focus:ring-blue-600 focus:ring-2 focus:outline-none"/>
-                    <button className="w-full sm:w-auto bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg transition cursor-pointer" onClick={handleAddTask}>Add Task</button>
-                </div>
-                {project.tasks.length === 0 ? (
-                    <p className="text-gray-600">No tasks available for this project.</p>
-                ) : (
-                    <div className="space-y-2">
-                        {project.tasks.map(task => (
-                            <TaskItem key={task.id} task={task} toggleTask={() => toggleTask(project.id, task.id)} deleteTask={() => deleteTask(project.id, task.id)}/>
-                        ))}
+                <div className="bg-white rounded-xl shadow-md p-4 sm:p-6 space-y-4">
+                    <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-4">
+                        <div className="flex-1 min-w-0">
+                            <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800 break-words">{project.name}</h1>
+                            <p className="text-sm sm:text-base text-gray-600 mt-2 break-words">{project.description}</p>
+                            <p className="text-xs sm:text-sm text-gray-500 mt-2">
+                            Created on: <span className="font-medium">{project.createdAt}</span>
+                            </p>
+                        </div>
+                        <div className="gap-3 flex flex-col items-start sm:items-end sm:gap-10 flex-shrink-0">
+                            <span className={`px-3 py-1 rounded-full text-xs sm:text-sm font-semibold whitespace-nowrap
+                                ${project.status === "Completed" ? "bg-green-500 text-white" : 
+                                project.status === "In Progress" ? "bg-blue-500 text-white" : "bg-yellow-500 text-white"}`}>
+                                {project.status}
+                            </span>
+                            <button onClick={() => setIsModalOpen(true)} className="w-full sm:w-auto px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-sm font-semibold">Edit</button>
+                        </div>
                     </div>
-                )}
-            </div>
-        </div>       
-    )
+                    
+                    
+                </div>
+                <h3 className="text-xl font-bold mt-6 mb-2">Progress</h3>
+                <div className="mt-2 justify-items-center">
+                    <ProgressBar value={tasksProgress(project.tasks)} />
+                </div>
+                <div className="mt-8">
+                    <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4 gap-4">
+                        <h3 className="text-xl font-bold">Tasks</h3>
+                        <input type="text" placeholder="New Task?" value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} className="w-full sm:flex-1 border rounded-md bg-gray-100 p-2 focus:ring-blue-600 focus:ring-2 focus:outline-none"/>
+                        <button className="w-full sm:w-auto bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg transition cursor-pointer" onClick={handleAddTask}>Add Task</button>
+                    </div>
+                    {project.tasks.length === 0 ? (
+                        <p className="text-gray-600">No tasks available for this project.</p>
+                    ) : (
+                        <div className="space-y-2">
+                            {project.tasks.map(task => (
+                                <TaskItem key={task.id} task={task} toggleTask={() => toggleTask(project.id, task.id)} deleteTask={() => deleteTask(project.id, task.id)}/>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>       
+        )
+    }
 }
 export default ProjectDetails;
